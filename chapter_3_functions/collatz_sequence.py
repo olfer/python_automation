f11c14a9770edd534faba68e072b095efe023f10
Olfer <karl489755@gmail.com>
2022-05-02 18:56:29 +0300

    добавлена возможность повторного запуска функции из консоли

--- chapter_3_functions/collatz_sequence.py
+++ chapter_3_functions/collatz_sequence.py
@@ -1,16 +1,27 @@
  # последовательность коллатца
 
 def collatz(number):
+    print('Введите число для последовательности Коллатца')
+    number = int(input())
     while number != 1:
         if number % 2 == 0:
             number = number // 2
         else:
             number = 3 * number + 1
         print(number)
+    print('Исследовать еще одну последовательность? (Введите Да или Нет)')
+    repeat = input()
+    if repeat == 'да':
+        collatz(number)
+    else:
+        print('Надеюсь все прошло хорошо!')
 
-print('Введите число для последовательности Коллатца')
-number = int(input())
+
+repeat = ''
+number = 0
 collatz(number)
+
+
 
 
 
