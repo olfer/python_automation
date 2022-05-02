b822df233ed56e02f0c9836fdf09175a907b7215
Olfer <karl489755@gmail.com>
2022-05-02 14:43:19 +0300

    последовательность коллатца отлично работает

--- chapter_3_functions/collatz_sequence.py
+++ chapter_3_functions/collatz_sequence.py
@@ -1,1 +1,16 @@
-#  последовательность коллатца
+ # последовательность коллатца
+
+def collatz(number):
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+        else:
+            number = 3 * number + 1
+        print(number)
+
+print('Введите число для последовательности Коллатца')
+number = int(input())
+collatz(number)
+
+
+
